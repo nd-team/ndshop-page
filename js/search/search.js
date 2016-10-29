f9d956a94f7ff58b4019971293b12d27be306ddd
lijianhu1 <403056352@qq.com>
2016-10-29 18:17:30 +0800

    do allorder.html

--- js/search/search.js
+++ js/search/search.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+    $('.sear-page ul li a').click(function(){
+        $(this).parent().addClass('active').siblings('li').removeClass('active');
+    });
+    var current
+    $('.sear-page .last').click(function(){
+        current = $('.sear-page .active').index();
+        current--;
+        if(current <= 0){
+            current = 0;
+        }
+        $('.sear-page ul li').eq(current).addClass('active').siblings('li').removeClass('active');
+    });
 
     $('.brand-con li a:not(:last)').click(function(){
         $(this).parent().addClass('active').siblings().removeClass('active')
@@ -6,8 +18,7 @@ $(document).ready(function(){
 
     $('.sear-filter a').click(function(){
         $(this).parent().addClass('active').siblings('li').removeClass('active');
-    })
-
+    });
 
 
     $('.sear-page ul li a').click(function(){
@@ -27,5 +38,9 @@ $(document).ready(function(){
         currentPage = $('.sear-page .active').index();
         currentPage++;
         $('.sear-page ul li').eq(currentPage).addClass('active').siblings('li').removeClass('active');
+
     });
+    $('.order-recommend .recom-page button').click(function(){
+
+    })
 });
