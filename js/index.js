6f0770bd61fb2afb36768395586ff1b2a96a9b9a
daixiuhua <1293225334@qq.com>
2016-11-04 17:50:16 +0800

    modify all pages and style and add banner auto

--- js/index.js
+++ js/index.js
@@ -1,15 +1,43 @@
 
 $(document).ready(function(){
-
-    function banner_pic_change() {
-        a = 0
-        $(".banner_pic_ul li").eq(i).animate({
-            opacity: "1"
-        }).siblings().animate({
-            opacity: "0"
-        }, function() {
-            a = 1;
-        });
-        $(".banner_but_ul li").eq(i).css("background", "#B1191A").siblings().css("background", "#3E3E3E")
+    var i = 0;
+    var a = 1;
+    var all_li = $(".banner-pic li").length - 1;
+    var circleLi=$(".banner-circle li");
+    var key=0;/*控制图片张数*/
+    circleLi.on("click",function(){
+        i=key=$(this).index();
+        banner_change();
+        circles();
+    });
+    function banner_change() {
+    a = 0;
+    $(".banner-pic li").eq(i).animate({
+        opacity: "1"
+    }).siblings().animate({
+        opacity: "0"
+    }, function() {
+        a = 1;
+    });
+    $(".banner-circle li").eq(i).css("background", "#dce2e2").siblings().css("background", "#b4b5b7")
     }
+    banner_change();
+    /*添加小圆点函数*/
+    function circles(){
+        circleLi.eq(i).addClass('current').siblings().removeClass('current');
+    }
+    function banner_change_self() {
+        i++;
+        if (i > all_li) {
+            i = 0;
+        }
+        banner_change()
+    }
+    banner_change_time = setInterval(banner_change_self, 2000);
+    $(".main-banner").on("mouseover", function() { //鼠标进入轮播区域时，自动切换暂停
+        clearInterval(banner_change_time);
+    })
+    $(".main-banner").on("mouseout", function() { //鼠标离开轮播区域时，自动切换继续
+        banner_change_time = setInterval(banner_change_self, 2000);
+    })
 })
