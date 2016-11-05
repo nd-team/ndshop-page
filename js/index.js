8a3f0b16d43d98d4b92f161e6bf60ef0b1268171
daixiuhua <1293225334@qq.com>
2016-11-05 14:26:38 +0800

    modify pop style

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
     }, function() {
         a = 1;
     });
-    $(".banner-circle li").eq(i).css("background", "#dce2e2").siblings().css("background", "#b4b5b7")
+    $(".banner-circle li").eq(i).css("background", "#dce2e2").siblings().css("background", "#b4b5b7");
     }
     banner_change();
     /*添加小圆点函数*/
@@ -31,7 +31,7 @@ $(document).ready(function(){
         if (i > all_li) {
             i = 0;
         }
-        banner_change()
+        banner_change();
     }
     var banner_timer= setInterval(banner_change_self, 2000);
     $(".main-banner").on("mouseover", function() { //鼠标进入轮播区域时，自动切换暂停
@@ -40,4 +40,20 @@ $(document).ready(function(){
     $(".main-banner").on("mouseout", function() { //鼠标离开轮播区域时，自动切换继续
         banner_timer = setInterval(banner_change_self, 2000);
     })
+
+    // 鼠标弹窗效果
+
+    $('.sidebar-list').on("mouseover",function (e) {
+           
+        $(this).find('.same-pw').show();
+        e.stopPropagation();
+        e.preventDefault();
+    })
+    $('.sidebar-list').on("mouseout",function (e) {
+        $('.same-pw').hide();
+        e.stopPropagation();
+        e.preventDefault();
+    })
+
+
 })
