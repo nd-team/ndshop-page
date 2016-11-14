d47b52e8f48ae20c0c13be1493f6e7836d8e919b
lijianhu1 <403056352@qq.com>
2016-11-14 17:33:22 +0800

    finish logistics

---
+++ js/business/business.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    //模态框
+    $('.newbtn a').on('click',function(e){
+        $('body').css("overflow","hidden")
+        $('#modal').show();
+        $('#modalbg').show();
+        e.stopPropagation();
+    });
+    $('#modal .m-close').click(function(e){
+        $('body').css("overflow","auto")
+        $('#modal').hide();
+        $('#modalbg').hide();
+        e.stopPropagation();
+    });
+    $('#modal .edit a').click(function(){
+        $('#submodal').show();
+        $('#submodalbg').show();
+    })
+    $('#submodal .subclose').click(function(){
+        $('#submodal').hide();
+        $('#submodalbg').hide();
+    })
+});
--- js/common.js
+++ js/common.js
@@ -1,6 +1,9 @@
-var ajax = new XMLHttpRequest();
-ajax.open("GET", "/font/icons/svg-symbols.svg", true);
-ajax.onload = function(e) {
-    document.body.insertAdjacentHTML("afterBegin", '<div style="display:none;">' + ajax.responseText + '</div>');
+window.onload=function(){
+    var ajax = new XMLHttpRequest();
+    ajax.open("GET", "/font/icons/svg-symbols.svg", true);
+    ajax.onload = function(e) {
+        document.body.insertAdjacentHTML("afterBegin", '<div style="display:none;">' + ajax.responseText + '</div>');
+    }
+    ajax.send();
 }
-ajax.send();
+
